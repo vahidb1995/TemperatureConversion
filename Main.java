678d7911f5b6d08d0f590bde9c5501d8b8939513
Vahid <bahramivahid@outlook.com>
2025-03-23 22:54:09 +0330

    crate frame

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    JFrame frame = new JFrame("Temperature Conversion");
+    frame.setSize(420, 420);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    //frame.getContentPane().setBackground(Color.lightGray);
   }
 }
